85ed05601c4464ef17a735f019cc40e21712a1b1
Brok3nBass <35891882+Brok3nBass@users.noreply.github.com>
2018-03-06 14:35:43 +0100

    mehhh
    blegh

--- DiceStreakDetector/src/dicestreakdetector/DiceStreakDetector.java
+++ DiceStreakDetector/src/dicestreakdetector/DiceStreakDetector.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package dicestreakdetector;
+import java.util.Random;
 
 /**
  *
@@ -15,7 +16,18 @@ public class DiceStreakDetector {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int amountOfThrows = 20;
+    int numbers[] = null;
+    for (int x = 0; x < amountOfThrows; x++){
+        numbers[x] = ThrowDice();
+    }
+    for (int x = 0; x < numbers.length - 1; x++){
+        System.out.println(numbers[x]);
+    }
+    }
+    public static int ThrowDice(){
+        int DiceThrow = new Random().nextInt(6);
+        return DiceThrow;
     }
     
 }
