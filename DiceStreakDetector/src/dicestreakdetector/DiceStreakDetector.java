55a2af379874c1fc5a729f6f502c90ffc034470a
Wesdewess <35997013+Wesdewess@github.com>
2018-03-06 14:29:07 +0100

    jkvs

--- DiceStreakDetector/src/dicestreakdetector/DiceStreakDetector.java
+++ DiceStreakDetector/src/dicestreakdetector/DiceStreakDetector.java
@@ -5,17 +5,33 @@
  */
 package dicestreakdetector;
 
+import java.util.Random;
+
 /**
  *
  * @author Wessel
  */
 public class DiceStreakDetector {
-
+int randomdobbel;
+int rnd;
+int dobbels[];
+int nr;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+   
+        
+       
     }
-    
+     
 }
+
+
+
+
+/*
+rnd = new Random().nextInt(6);
+       dobbels[nr] = rnd;
+*/
