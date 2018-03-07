2eb594dc632c08afd6a22ccc6db5bf6a395471bb
Wesdewess <35997013+Wesdewess@github.com>
2018-03-07 19:10:19 +0100

    alles werkt
    maak het maar mooier als je wilt

--- src/dicestreakdetector/Dicestreakdetector.java
+++ src/dicestreakdetector/Dicestreakdetector.java
@@ -25,33 +25,82 @@ public class Dicestreakdetector {
     // Generate the ArrayList Throws of {lenth} long
     for (int x = 0; x < length; x++){
         Throws.add(ThrowDice());
+        
         }
     printStuff(Throws); // Print ArrayList for DebuggingPurposes 
     }
     // ThrowDice Method for generating a number between 1 and 6
     public static int ThrowDice(){
         int DiceThrow = new Random().nextInt((6 - 1)+ 1 ) + 1;
+        System.out.print(DiceThrow + "-");
         return DiceThrow;
     }
     // Print method for ArrayList
     public static void printStuff(ArrayList dobbels){{
-    for (int x = 0; x < dobbels.size(); x++){
-        if (dobbels.get(x+1) == dobbels.get(x+2)) {
-            System.out.print("(");
-            System.out.print(dobbels.get(x)+ " ");
-            System.out.println("SHIT1");
+    int x;
+        for (x = 1; x < dobbels.size()-2; x++){
+      
+        if (dobbels.get(x-1) == dobbels.get(x) && x == 1){
+            System.out.println();
+            System.out.print("( " + dobbels.get(x-1) + " ");
+            System.out.print(dobbels.get(x-1) + " ");
+        }
+        else {
+            if (x==1){
+                System.out.println();
+                System.out.print(dobbels.get(x-1) + " ");
+               if (dobbels.get(x) == dobbels.get(x+1)){
+                   System.out.print("( ");
+                   System.out.print(dobbels.get(x) + " ");
+               } else {
+                   System.out.print(dobbels.get(x) + " ");
+               }
+                
+            } else {
+                 System.out.print(dobbels.get(x)+ " ");
+            }
         }
         
-        else if (dobbels.get(x-1) == dobbels.get(x) && dobbels.get(x) != dobbels.get(x+1)) {
+          
+            
+           
+            
+       
+      if (dobbels.get(x-1) == dobbels.get(x) && dobbels.get(x) != dobbels.get(x+1)) {
             System.out.print(") ");
-            System.out.print(dobbels.get(x)+ " ");
-            System.out.println("SHIT");
+            
+            
         }
-        System.out.print("SHIT3");
         
+      if (dobbels.get(x+1) == dobbels.get(x+2) && dobbels.get(x) != dobbels.get(x+1)) {
+          if (x==1) { 
+          System.out.print("( ");
+         } else {
+              System.out.print("( ");
+          }
+      }
+      
+      }
+    for (x = dobbels.size()-2; x < dobbels.size()-1; x++){
+       
+        System.out.print(dobbels.get(x) + " ");
+        if (dobbels.get(x-1) == dobbels.get(x) && dobbels.get(x) != dobbels.get(x+1)) {
+            System.out.print(")");
+        }
         
+        System.out.print(dobbels.get(x+1) + " ");
+        if (dobbels.get(x) == dobbels.get(x+1)){
+            System.out.print(")");
+        }else {
+            
+        }
     }
     
-    }}
+      }
+    }
+    }
+    
+    
+    
 
-}
+    
